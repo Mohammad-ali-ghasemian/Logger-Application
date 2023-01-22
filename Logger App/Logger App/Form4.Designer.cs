9e5758563f896db215e3190d0c383b7025d53c5f
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2023-01-22 00:02:53 -0800

    update add member form controls

--- Logger App/Logger App/Form4.Designer.cs
+++ Logger App/Logger App/Form4.Designer.cs
@@ -28,23 +28,187 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.firstnameLbl = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lastnameLbl = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.birthYearLbl = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.salaryLbl = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.roleLbl = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.usernameLbl = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.passwordLbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // firstnameLbl
+            // 
+            this.firstnameLbl.AutoSize = true;
+            this.firstnameLbl.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstnameLbl.Location = new System.Drawing.Point(12, 22);
+            this.firstnameLbl.Name = "firstnameLbl";
+            this.firstnameLbl.Size = new System.Drawing.Size(69, 17);
+            this.firstnameLbl.TabIndex = 0;
+            this.firstnameLbl.Text = "Firstname";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(217, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 88);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(217, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // lastnameLbl
+            // 
+            this.lastnameLbl.AutoSize = true;
+            this.lastnameLbl.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastnameLbl.Location = new System.Drawing.Point(12, 70);
+            this.lastnameLbl.Name = "lastnameLbl";
+            this.lastnameLbl.Size = new System.Drawing.Size(67, 17);
+            this.lastnameLbl.TabIndex = 2;
+            this.lastnameLbl.Text = "Lastname";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(15, 232);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(217, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // birthYearLbl
+            // 
+            this.birthYearLbl.AutoSize = true;
+            this.birthYearLbl.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.birthYearLbl.Location = new System.Drawing.Point(12, 214);
+            this.birthYearLbl.Name = "birthYearLbl";
+            this.birthYearLbl.Size = new System.Drawing.Size(68, 17);
+            this.birthYearLbl.TabIndex = 4;
+            this.birthYearLbl.Text = "Birth Year";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(15, 279);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(217, 22);
+            this.textBox4.TabIndex = 7;
+            // 
+            // salaryLbl
+            // 
+            this.salaryLbl.AutoSize = true;
+            this.salaryLbl.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salaryLbl.Location = new System.Drawing.Point(12, 261);
+            this.salaryLbl.Name = "salaryLbl";
+            this.salaryLbl.Size = new System.Drawing.Size(46, 17);
+            this.salaryLbl.TabIndex = 6;
+            this.salaryLbl.Text = "Salary";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(15, 327);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(217, 22);
+            this.textBox5.TabIndex = 9;
+            // 
+            // roleLbl
+            // 
+            this.roleLbl.AutoSize = true;
+            this.roleLbl.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roleLbl.Location = new System.Drawing.Point(12, 309);
+            this.roleLbl.Name = "roleLbl";
+            this.roleLbl.Size = new System.Drawing.Size(36, 17);
+            this.roleLbl.TabIndex = 8;
+            this.roleLbl.Text = "Role";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(15, 136);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(217, 22);
+            this.textBox6.TabIndex = 11;
+            // 
+            // usernameLbl
+            // 
+            this.usernameLbl.AutoSize = true;
+            this.usernameLbl.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameLbl.Location = new System.Drawing.Point(12, 118);
+            this.usernameLbl.Name = "usernameLbl";
+            this.usernameLbl.Size = new System.Drawing.Size(70, 17);
+            this.usernameLbl.TabIndex = 10;
+            this.usernameLbl.Text = "Username";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(15, 184);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(217, 22);
+            this.textBox7.TabIndex = 13;
+            // 
+            // passwordLbl
+            // 
+            this.passwordLbl.AutoSize = true;
+            this.passwordLbl.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLbl.Location = new System.Drawing.Point(12, 166);
+            this.passwordLbl.Name = "passwordLbl";
+            this.passwordLbl.Size = new System.Drawing.Size(67, 17);
+            this.passwordLbl.TabIndex = 12;
+            this.passwordLbl.Text = "Password";
             // 
             // addMemberForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.ClientSize = new System.Drawing.Size(344, 461);
+            this.ClientSize = new System.Drawing.Size(244, 376);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.passwordLbl);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.usernameLbl);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.roleLbl);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.salaryLbl);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.birthYearLbl);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lastnameLbl);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.firstnameLbl);
             this.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.Name = "addMemberForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Logger - Add Member";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label firstnameLbl;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lastnameLbl;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label birthYearLbl;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label salaryLbl;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label roleLbl;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label usernameLbl;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label passwordLbl;
     }
 }
